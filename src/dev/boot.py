136bb5f10aefcff48a5259eb63f77b3783a90786
raratiru <tantiras@yandex.com>
2022-01-10 03:41:18 +0200

    Final Version

--- src/dev/boot.py
+++ src/dev/boot.py
@@ -7,12 +7,13 @@ execute as if running in a Django server.
 """
 
 import subprocess
+from os import environ
 from pathlib import Path
 from sys import path
 
 import click
-from click.decorators import help_option
 import django
+from click.decorators import help_option
 from django.conf import settings
 from django.core.management import call_command
 
@@ -42,10 +43,25 @@ def boot_django(app: str):
 
 
 @click.command()
-@click.option("--service", "-s",
-              default="makemigrations",
-              help="Specify service to run: (makemigrations(default), migrate, shell)")
-@click.argument("application_name", nargs=1)
+@click.option(
+    "-m",
+    "service",
+    flag_value="makemigrations",
+    help="Make migrations",
+    default=True,
+    show_default=True,
+)
+@click.option("-M", "service", flag_value="migrate", help="Migrate")
+@click.option("-s", "service", flag_value="shell", help="Run Django shell")
+@click.option(
+    "-n",
+    "application_name",
+    help="The name of the application or DEVAPP_NAME env variable.",
+    envvar="DEVAPP_NAME",
+    required=True,
+    # default=lambda: environ.get("DEVAPP_NAME", ""),
+    # prompt=True,
+)
 def control(application_name, service):
     boot_django(application_name)
     if service == "migrate":
