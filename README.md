d006eec838350df1ed62e647fa9a3a69f568b0a9
George Tantiras <raratiru@users.noreply.github.com>
2022-01-10 03:49:42 +0200

    Add Basic CLI (#1)
    * Working CLI
    
    * Final Version
    
    * Update README.md
    
    * Update README.md
    
    * Update setup.cfg

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# django-devapp
+# Django DevApp
 Basic tools to manage a django application outside of a Django project
+
+# Help
+```
+$ dev --help
+Usage: dev [OPTIONS]
+
+Options:
+  -m       Make migrations  [default: makemigrations]
+  -M       Migrate
+  -s       Run Django shell
+  -n TEXT  The name of the application or DEVAPP_NAME env variable.
+           [required]
+  --help   Show this message and exit.
+```
+# Example
+```
+$ export DEVAPP_NAME=app
+$ dev -s
+>>> 
+```
---
+++ src/dev/boot.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This file sets up and configures Django. It's used by scripts that need to
+execute as if running in a Django server.
+"""
+
+import subprocess
+from os import environ
+from pathlib import Path
+from sys import path
+
+import click
+import django
+from click.decorators import help_option
+from django.conf import settings
+from django.core.management import call_command
+
+
+def boot_django(app: str):
+    BASE_DIR = subprocess.run(["pwd"], capture_output=True, text=True).stdout.strip()
+    path.append(BASE_DIR)
+
+    settings.configure(
+        BASE_DIR=Path(BASE_DIR) / Path("people"),
+        DEBUG=True,
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": Path(BASE_DIR) / Path("db.sqlite3"),
+            }
+        },
+        INSTALLED_APPS=(
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            app,
+        ),
+        TIME_ZONE="UTC",
+        USE_TZ=True,
+    )
+    django.setup()
+
+
+@click.command()
+@click.option(
+    "-m",
+    "service",
+    flag_value="makemigrations",
+    help="Make migrations",
+    default=True,
+    show_default=True,
+)
+@click.option("-M", "service", flag_value="migrate", help="Migrate")
+@click.option("-s", "service", flag_value="shell", help="Run Django shell")
+@click.option(
+    "-n",
+    "application_name",
+    help="The name of the application or DEVAPP_NAME env variable.",
+    envvar="DEVAPP_NAME",
+    required=True,
+    # default=lambda: environ.get("DEVAPP_NAME", ""),
+    # prompt=True,
+)
+def control(application_name, service):
+    boot_django(application_name)
+    if service == "migrate":
+        call_command("migrate")
+    elif service == "shell":
+        call_command("shell")
+    else:
+        call_command("makemigrations", application_name)
